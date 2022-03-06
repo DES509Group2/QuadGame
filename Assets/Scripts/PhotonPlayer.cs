58cef1737c83a4857949f4b47d1a66edfe8b8665
多喝凉水。 <43728183+Folivora0707@users.noreply.github.com>
2022-03-06 14:33:38 +0000

    Team

--- Assets/Scripts/PhotonPlayer.cs
+++ Assets/Scripts/PhotonPlayer.cs
@@ -6,22 +6,73 @@ using UnityEngine;
 
 public class PhotonPlayer : MonoBehaviour
 {
-    private PhotonView PV;
-    public GameObject myAvatar; 
+    public PhotonView PV;
+    public GameObject myAvatar;
+    public int myTeam; 
 
     void Start()
     {
         PV = GetComponent<PhotonView>();
-        int spawnPicker = Random.Range(0, GameSetup.GS.spawnPoints.Length); 
         if (PV.IsMine)
         {
-            myAvatar = PhotonNetwork.Instantiate(Path.Combine("PhotonNetworkPrefabs", "Player"), 
-                GameSetup.GS.spawnPoints[spawnPicker].position, GameSetup.GS.spawnPoints[spawnPicker].rotation, 0);
+            PV.RPC("RPC_GetTeam", RpcTarget.MasterClient); 
         }
     }
 
     void Update()
     {
-        
+        if (myAvatar == null && myTeam != 0)
+        {
+            if (myTeam == 1)
+            {
+                int spawnPicker = Random.Range(0, GameSetup.GS.spawnPointsTeamOne.Length);
+                if (PV.IsMine)
+                {
+                    myAvatar = PhotonNetwork.Instantiate(Path.Combine("PhotonNetworkPrefabs", "Player"),
+                        GameSetup.GS.spawnPointsTeamOne[spawnPicker].position, GameSetup.GS.spawnPointsTeamOne[spawnPicker].rotation, 0);
+                }
+            }
+            if (myTeam == 2)
+            {
+                int spawnPicker = Random.Range(0, GameSetup.GS.spawnPointsTeamTwo.Length);
+                if (PV.IsMine)
+                {
+                    myAvatar = PhotonNetwork.Instantiate(Path.Combine("PhotonNetworkPrefabs", "Player"),
+                        GameSetup.GS.spawnPointsTeamTwo[spawnPicker].position, GameSetup.GS.spawnPointsTeamTwo[spawnPicker].rotation, 0);
+                }
+            }
+            if (myTeam == 3)
+            {
+                int spawnPicker = Random.Range(0, GameSetup.GS.spawnPointsTeamThree.Length);
+                if (PV.IsMine)
+                {
+                    myAvatar = PhotonNetwork.Instantiate(Path.Combine("PhotonNetworkPrefabs", "Player"),
+                        GameSetup.GS.spawnPointsTeamThree[spawnPicker].position, GameSetup.GS.spawnPointsTeamThree[spawnPicker].rotation, 0);
+                }
+            }
+            if (myTeam == 4)
+            {
+                int spawnPicker = Random.Range(0, GameSetup.GS.spawnPointsTeamFour.Length);
+                if (PV.IsMine)
+                {
+                    myAvatar = PhotonNetwork.Instantiate(Path.Combine("PhotonNetworkPrefabs", "Player"),
+                        GameSetup.GS.spawnPointsTeamFour[spawnPicker].position, GameSetup.GS.spawnPointsTeamFour[spawnPicker].rotation, 0);
+                }
+            }
+        }
+    }
+
+    [PunRPC]
+    void RPC_GetTeam()
+    {
+        myTeam = GameSetup.GS.nextPlayersTeam;
+        GameSetup.GS.UpdateTeam();
+        PV.RPC("RPC_SentTeam", RpcTarget.OthersBuffered, myTeam); 
+    }
+
+    [PunRPC]
+    void RPC_SentTeam(int whichTeam)
+    {
+        myTeam = whichTeam; 
     }
 }
